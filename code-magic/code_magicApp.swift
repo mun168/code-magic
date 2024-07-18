ac20a47234584baaea05503a8a33989c9f9fa0fe
mun168 <67210857+mun168@users.noreply.github.com>
2024-07-18 12:25:30 +0200

    Initial Commit

---
+++ code-magic/code_magicApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_magicApp.swift
+//  code-magic
+//
+//  Created by Munashe Chibaya on 18/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct code_magicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
